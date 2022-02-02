502f71ce24916e127158581f3239399eecbfb8d8
hamidehalaee <hamidehalaeeyasar@gmail.com>
2022-02-02 13:23:51 -0800

    add project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+            <script src="{{ asset('js/app.js') }}" defer></script>
+            <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    </head>
+    <body class="antialiased">
+
+            <div class="container">
+                <!-- Grey with black text -->
+                <nav class="navbar navbar-expand-sm bg-light navbar-light">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Active</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Disabled</a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <!-- Black with white text -->
+                <nav class="navbar navbar-expand-sm bg-dark navbar-dark">...</nav>
+
+                <!-- Blue with white text -->
+                <nav class="navbar navbar-expand-sm bg-primary navbar-dark">...</nav>
+            </div>
+    </body>
+</html>
